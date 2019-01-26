46c4c7ce1fadd2fa94202f24fcbdd5476266f4f6
Michael Ninh <michaelninh@Michale-Ninh-MacBook-Pro.local>
2019-01-26 12:11:14 -0800

    turned off debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x(wfct&2#3$yt8f83yt@kp88951z$v)o4ywa@+*8d#73t%&vq1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
